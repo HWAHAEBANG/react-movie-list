22bb7197ed512d0f3eea53aab8d4d4df802defab
화해방 <bcl0206@naver.com>
2022-10-07 17:49:12 +0900

    Learning How To Use useEffect Function

--- src/App.js
+++ src/App.js
@@ -1,13 +1,45 @@
-import Button from "./Button";
-import styles from "./App.module.css";
+// import Button from "./Button";
+// import styles from "./App.module.css";
+import { useState, useEffect } from "react"; 
 
 function App() {
+  const [counter, setValue] = useState(0);
+  const [search, setSearch] = useState("");
+  const onClick = () => setValue((prev) => prev + 1); 
+  const onChange = (event) => setSearch((event.target.value))
+  // console.log("I run all the time");
+  
+  useEffect(() => {
+    console.log("CALL THE API..");
+    console.log("I run only once.");
+  }, []);
+  
+  useEffect(() => {
+    if(search !=="" && search.length > 5){
+      console.log("SEARCH FOR", search); 
+      console.log("I run when 'search' changes.");
+    }
+  }, [search]);
+
+  useEffect(() => {
+    if(counter >= 1){
+      console.log("COUNT UP!!");
+      console.log("I run when 'counter'is clicked.");
+    }
+  }, [counter])
+
+
+  useEffect(() => {
+      console.log("I run when 'counter' is clicked & counter change");
+  }, [counter, search])
+
   return (
     <div>
-      <h1 className={styles.title}>Welcome back!!!</h1>
-      <Button text={"Continue"}/>
+      <input value={search} onChange={onChange} type = "text" placeholder = "Search here..."/>
+      <h1>{counter}</h1>
+      <button onClick = {onClick}>click me</button>
     </div>
   ); 
 }
 
-export default App;
+export default App; 
