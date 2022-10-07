9a3a684327edb2bbedc31788737e856c5981b9c3
화해방 <bcl0206@naver.com>
2022-10-07 21:55:07 +0900

    make the todolist

--- src/App.js
+++ src/App.js
@@ -2,53 +2,36 @@
 // import styles from "./App.module.css";
 import { useState, useEffect} from "react"; 
 
-function Hello(){
-  // 방법1
-  //   useEffect(()=>{
-  //   console.log('created :>');
-  //   return() => console.log("destroyed :<");
-  // }, [])
-  // return <h1>Hello</h1>;
-
-  // 방법2 : 함수를 미리 다 만들어 놓고 호출해서 쓰기
-  // function byeFn(){
-  //   console.log("bye :<");
-  // }
-
-  // function hiFn(){
-  //   console.log("created :>");
-  //   return byeFn;
-  // }
-
-  // useEffect(hiFn,[]);
-  // return <h1>Hello</h1>;
-
-  // 방법3 : 익명함수를 사용하여 짧게 나타내기
-  // useEffect(function(){
-  //   console.log("hi :>");
-  //   return function () {
-  //     console.log("bye :<");
-  //   };
-  // }, []); 
-  // return <h1>Hello</h1>;
-
-  // 방법4 : 익명함수를 arrow function으로 바꾸어 더 짧게 나타내기
-  useEffect(()=>{
-    console.log("hi :>");
-    return () => console.log("bye :<");
-  },[])
-  return <h1>Hello</h1>;
-}
-
-
-
 function App() {
- const [showing, setShowing] = useState(false);
- const onClick = () => setShowing((prev) => !prev);
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if(toDo===""){
+      return;
+    }
+    console.log(toDo);
+    setToDo("");
+    setToDos(currentArray => [toDo,...currentArray])
+  };
+  console.log(toDos);
+
   return (
     <div>
-      {showing? <Hello/> : null}
-      <button onClick = {onClick}>{showing ? "Hide" : "Show"}</button>
+      <h1>To Do List({toDos.length})</h1>
+      <form onSubmit = {onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          placeholder="Write your to do..."
+        />
+        <button>Add To Do</button>
+      </form>
+      <hr/>
+      <ul>
+        {toDos.map((item, index) => <li key = {index}>{item}</li>)}
+      </ul>
     </div>
   ); 
 }
