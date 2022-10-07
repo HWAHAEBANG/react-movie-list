8a7ffa03044475c33c6049e639a3ac6e53f419fa
화해방 <bcl0206@naver.com>
2022-10-07 18:44:12 +0900

    Learning How To Use cleanup

--- src/App.js
+++ src/App.js
@@ -1,43 +1,54 @@
 // import Button from "./Button";
 // import styles from "./App.module.css";
-import { useState, useEffect } from "react"; 
+import { useState, useEffect} from "react"; 
+
+function Hello(){
+  // 방법1
+  //   useEffect(()=>{
+  //   console.log('created :>');
+  //   return() => console.log("destroyed :<");
+  // }, [])
+  // return <h1>Hello</h1>;
+
+  // 방법2 : 함수를 미리 다 만들어 놓고 호출해서 쓰기
+  // function byeFn(){
+  //   console.log("bye :<");
+  // }
+
+  // function hiFn(){
+  //   console.log("created :>");
+  //   return byeFn;
+  // }
+
+  // useEffect(hiFn,[]);
+  // return <h1>Hello</h1>;
+
+  // 방법3 : 익명함수를 사용하여 짧게 나타내기
+  // useEffect(function(){
+  //   console.log("hi :>");
+  //   return function () {
+  //     console.log("bye :<");
+  //   };
+  // }, []); 
+  // return <h1>Hello</h1>;
+
+  // 방법4 : 익명함수를 arrow function으로 바꾸어 더 짧게 나타내기
+  useEffect(()=>{
+    console.log("hi :>");
+    return () => console.log("bye :<");
+  },[])
+  return <h1>Hello</h1>;
+}
+
+
 
 function App() {
-  const [counter, setValue] = useState(0);
-  const [search, setSearch] = useState("");
-  const onClick = () => setValue((prev) => prev + 1); 
-  const onChange = (event) => setSearch((event.target.value))
-  // console.log("I run all the time");
-  
-  useEffect(() => {
-    console.log("CALL THE API..");
-    console.log("I run only once.");
-  }, []);
-  
-  useEffect(() => {
-    if(search !=="" && search.length > 5){
-      console.log("SEARCH FOR", search); 
-      console.log("I run when 'search' changes.");
-    }
-  }, [search]);
-
-  useEffect(() => {
-    if(counter >= 1){
-      console.log("COUNT UP!!");
-      console.log("I run when 'counter'is clicked.");
-    }
-  }, [counter])
-
-
-  useEffect(() => {
-      console.log("I run when 'counter' is clicked & counter change");
-  }, [counter, search])
-
+ const [showing, setShowing] = useState(false);
+ const onClick = () => setShowing((prev) => !prev);
   return (
     <div>
-      <input value={search} onChange={onChange} type = "text" placeholder = "Search here..."/>
-      <h1>{counter}</h1>
-      <button onClick = {onClick}>click me</button>
+      {showing? <Hello/> : null}
+      <button onClick = {onClick}>{showing ? "Hide" : "Show"}</button>
     </div>
   ); 
 }
