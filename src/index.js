6a50a7c20b0169110d30a81cc6cc1ac5fb42f152
화해방 <bcl0206@naver.com>
2022-10-18 17:20:35 +0900

    detail tryry

--- src/index.js
+++ src/index.js
@@ -5,8 +5,8 @@ import "./styles.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  // <React.StrictMode>
+  <App />
+  // </React.StrictMode>
+  // document.getElementById("root")
 );
--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,19 +1,27 @@
 import { useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { json, useParams } from "react-router-dom";
 
 function Detail() {
+  const test = "";
+  const name = "방충림";
   const { id } = useParams();
-  //console.log(id);
+  console.log(id);
   const getMovie = async () => {
     const json = await (
       await fetch(`https://yts.mx/api/v2/movie_details.json?movie_id=${id}`)
     ).json();
-    console.log(json);
+    const test = json.data.movie.id;
   };
   useEffect(() => {
     getMovie();
   }, []);
-  return <h1>detail</h1>;
+  return (
+    <div>
+      <h1>detail</h1>
+      <div>{name}</div>
+      <div>{test}</div>
+    </div>
+  );
 }
 
 export default Detail;
